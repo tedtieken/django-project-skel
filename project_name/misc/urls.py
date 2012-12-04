fa7ef58110a51a973d888884eb8153b676d768b4
Ted <ted.tieken@gmail.com>
2012-12-04 13:36:16 -0500

    Removing base.html, adding home.html (including a functional base will require the verbatim tag from 1.5). Enabling homepage by default

--- project_name/misc/urls.py
+++ project_name/misc/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('{{ project_name }}.misc.views',
-  #url(r'^$', 'home', name='home'),
+  url(r'^$', 'home', name='home'),
 )
