26c1ddf2b87c314b9ef174cda62067503038096f
Ted <ted.tieken@gmail.com>
2012-12-05 20:10:11 -0500

    Minor changes.

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('{{ project_name }}.misc.urls')),
-    (r'^accounts/', include('allauth.urls')),
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^', include('{{ project_name }}.misc.urls')),
+  url(r'^accounts/', include('allauth.urls')),
 )
